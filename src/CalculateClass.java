5335106597b14573b17cbf667068bf4b2fb0d520
PetrovitchSharp <korchagin_official@yahoo.com>
2021-11-09 11:23:10 +0400

    Added functions for calculation of theoretical characteristics

--- src/CalculateClass.java
+++ src/CalculateClass.java
@@ -56,6 +56,90 @@ public class CalculateClass {
     }
 
     /**
+     * Расчет теоретического мат. ожидания
+     * @param proc Параметры СП
+     * @return Теоретическое мат. ожидание
+     */
+    public static double getMathematicalExpectation(RandomProcess proc){
+        var mathExp = 0.;
+
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Равномерное")){
+            mathExp = (proc.getDistributionLaw().getLeft() + proc.getDistributionLaw().getRight()) / 2;
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Нормальное")){
+            mathExp = proc.getDistributionLaw().getExpectedValue();
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Показательное")){
+            mathExp = 1. / proc.getDistributionLaw().getIntensity();
+        }
+
+        return mathExp;
+    }
+
+    /**
+     * Расчет теоретической дисперсии
+     * @param proc Параметры СП
+     * @return Теоретическая дисперсия
+     */
+    public static double getTheoreticalDispertion(RandomProcess proc){
+        var disp = 0.;
+
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Равномерное")){
+            disp = Math.pow(proc.getDistributionLaw().getRight() - proc.getDistributionLaw().getLeft(), 2) / 12;
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Нормальное")){
+            disp = proc.getDistributionLaw().getDispersion();
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Показательное")){
+            disp = 1. / Math.pow(proc.getDistributionLaw().getIntensity(),2);
+        }
+
+        return disp;
+    }
+
+    /**
+     * Расчет теоретического коэффициента асимметрии
+     * @param proc Параметры СП
+     * @return Теоретический коэффициент асимметрии
+     */
+    public static double getTheoreticalAsymmetryCoefficient(RandomProcess proc){
+        var asymCoef = 0.;
+
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Равномерное")){
+            asymCoef = 0;
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Нормальное")){
+            asymCoef = 0;
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Показательное")){
+            asymCoef = 2;
+        }
+
+        return asymCoef;
+    }
+
+    /**
+     * Расчет теоретического коэффициента эксцесса
+     * @param proc Параметры СП
+     * @return Теоретический коэффициент эксцесса
+     */
+    public static double getTheoreticalKurtosisCoefficient(RandomProcess proc){
+        var mathExp = 0.;
+
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Равномерное")){
+            mathExp = -1.2;
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Нормальное")){
+            mathExp = 0;
+        }
+        if (proc.getDistributionLaw().getKindDistributionLaw().equals("Показательное")){
+            mathExp = 6;
+        }
+
+        return mathExp;
+    }
+
+    /**
      * Расчет выборочного среднего
      * @param proc Случайный процесс
      * @return Выборочное среднее
@@ -69,7 +153,7 @@ public class CalculateClass {
      * @param proc Случайный процесс
      * @return Выборочная несмещенная дисперсия
      */
-    public static double getDispertion(double[] proc){
+    public static double getExperimentalDispertion(double[] proc){
         var n = proc.length;
 
         return ((double) n / (n - 1))*getCentralMoment(proc,2);
@@ -80,7 +164,7 @@ public class CalculateClass {
      * @param proc Случайный процесс
      * @return Выборочный несмещенный коэффициент асимметрии
      */
-    public static double getAsymmetryCoefficient(double[] proc){
+    public static double getExperimentalAsymmetryCoefficient(double[] proc){
         var n = proc.length;
 
         return (Math.sqrt(n*(n-1))/(n-2))*getCentralMoment(proc,3)
@@ -92,7 +176,7 @@ public class CalculateClass {
      * @param proc Случайный процесс
      * @return Выборочный несмещенный коэффициент эксцесса
      */
-    public static double getKurtosisCoefficient(double[] proc){
+    public static double getExperimentalKurtosisCoefficient(double[] proc){
         var n = proc.length;
 
         return (Math.pow(n,2)-1)/((n-2)*(n-3))
