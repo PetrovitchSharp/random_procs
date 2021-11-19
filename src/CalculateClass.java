cc8955ab68b036e20c536adec012b8a5a6f9ca52
PetrovitchSharp <korchagin_official@yahoo.com>
2021-11-19 14:46:46 +0400

    Added chi2 test and fixed KS test

--- src/CalculateClass.java
+++ src/CalculateClass.java
@@ -210,22 +210,65 @@ public class CalculateClass {
         return (int)Math.round(Math.exp(1)*Math.log(numOfSamples));
     }
 
-    public static int getNumOfCorrFunctionSamples(CorrelationFunction corrFunc, int numOfSaamples){
+    /**
+     * Расчет количества отсчетов для графика КФ
+     * @param corrFunc Параметры КФ
+     * @return Количество отсчетов для графика КФ
+     */
+    public static int getNumOfCorrFunctionSamples(CorrelationFunction corrFunc){
         return 0; //TODO: Найти формулы для количества отсчетов, необходимого для построения корр функции
     }
 
     /**
      * Тест Колмогорова-Смирнова
      * @param procSample Выборка точек из СП
-     * @param realFunction Теоретическая функция
+     * @param realFunction Теоретическая функция (ФР или КФ)
      * @param checkParams Параметры для теста
+     * @param typeOfGenerating Тип генерации СП (0 - ФР, 1 - КФ)
      * @return Пройден \ Не пройден тест
      */
-    public static boolean KSTest(double[] procSample, Function2D realFunction, HypothesisCheck checkParams){
+    public static boolean KSTest(double[] procSample, Function2D realFunction, HypothesisCheck checkParams, int typeOfGenerating){
         var table = new KSTable();
+        XYSeries subsample;
 
-        var testValue = KSTestValue(procSample, realFunction);
-        var critValue = table.getCritivalValue(checkParams.getNumberOfDegreesOfFreedom(), checkParams.getSignificanceLevel());
+        if (typeOfGenerating == 0) {
+            subsample = getDistributionFunctionData(
+                    procSample,
+                    checkParams.getNumberOfDegreesOfFreedom())
+                    .getSeries(0);
+        }
+        else {
+            subsample = getCorrelationFunctionData(
+                    procSample,
+                    checkParams.getNumberOfDegreesOfFreedom())
+                    .getSeries(0);
+        }
+
+
+        var testValue = KSTestValue(subsample, realFunction);
+        var critValue = table.getCritivalValue(
+                checkParams.getNumberOfDegreesOfFreedom(),
+                checkParams.getSignificanceLevel()
+        );
+
+        return testValue <= critValue;
+    }
+
+    /**
+     * Тест Хи-квадрат (неприменим для случая КФ)
+     * @param procSample СП
+     * @param realFunction Теоретическая функция
+     * @param checkParams Параметры для теста
+     * @return Пройден \ не пройден тест
+     */
+    public static boolean Chi2Test(double[] procSample, Function2D realFunction, HypothesisCheck checkParams){
+        var table = new PearsonTable();
+
+        var testValue = chi2TestValue(procSample, realFunction, checkParams.getNumberOfDegreesOfFreedom());
+        var critValue = table.getCritivalValue(
+                checkParams.getNumberOfDegreesOfFreedom(),
+                checkParams.getSignificanceLevel()
+        );
 
         return testValue <= critValue;
     }
@@ -344,15 +387,69 @@ public class CalculateClass {
      * @param realFunction Теоретическая функция
      * @return Значение статистики
      */
-    private static double KSTestValue(double[] procSample, Function2D realFunction){
-        var len = procSample.length;
+    private static double KSTestValue(XYSeries procSample, Function2D realFunction){
+        var len = procSample.getItemCount();
         var d = new double[len];
 
         for (var i = 0; i < len; i++){
-            d[i] = Math.max(procSample[i] - realFunction.getValue(i), realFunction.getValue(i) - procSample[i]);
+            d[i] = Math.max(
+                    (double) procSample.getY(i) - realFunction.getValue((double) procSample.getX(i)),
+                    realFunction.getValue((double) procSample.getX(i)) - (double) procSample.getY(i)
+            );
         }
 
         return StatUtils.max(d);
+    }
+
+    /**
+     * Вычисление статистики для критерия Хи-квадрат
+     * @param proc СП
+     * @param realFunction Теоретическая функция
+     * @param freedomDegrees Число степеней сводобы
+     * @return Значение статистики
+     */
+    private static double chi2TestValue(double[] proc, Function2D realFunction, int freedomDegrees){
+        var max = StatUtils.max(proc);
+        var min = StatUtils.min(proc);
+        Arrays.sort(proc);
+        var shift = (max - min) / (freedomDegrees + 1);
+        var statistic = 0.;
+
+        while (min <= max - shift){
+            var expDensity = experimentalDensity(proc, min, min +shift);
+            var realDensity = realFunction.getValue(min +shift) - realFunction.getValue(min);
+            if (min == max-shift){
+                expDensity++;
+            }
+            statistic += Math.pow(expDensity - realDensity, 2)/ realDensity;
+            min += shift;
+        }
+
+        return statistic * (freedomDegrees + 2);
+    }
+
+    /**
+     * Вычисление экспериментальной плотности
+     * @param proc СП
+     * @param min Левая граница интервала
+     * @param max Правая граница интервала
+     * @return Экспериментальная плотность на интервале
+     */
+    private static double experimentalDensity(double[] proc, double min, double max){
+        var counter = 0;
+        for (double v : proc) {
+            if (v < min) {
+                continue;
+            }
+            if (v >= max) {
+                break;
+            }
+            if (v >= min) {
+                counter++;
+            }
+        }
+
+        return (double) counter / proc.length;
     }
 
     /**
